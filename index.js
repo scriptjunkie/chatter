43e6b489e90a1b53a17dcef5a8008d9001c83e56
scriptjunkie <scriptjunkie@scriptjunkie.us>
2024-12-25 13:51:36 -0500

    Export context for debugging.

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ loadpwbutton.onclick=async ()=> context.set_keys_from_password(pwbox.value);
 savepwbutton.onclick=async ()=> context.export_keys_with_password(pwbox.value);
 
 console.log('starting');
+
+window.context = context;

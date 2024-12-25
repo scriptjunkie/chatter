239a5d1a70e031970f1d09808debcff9a312820c
scriptjunkie <scriptjunkie@scriptjunkie.us>
2024-12-25 13:47:43 -0500

    clear line

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { context } from 'chattier';
 
-
 loadpwbutton.onclick=async ()=> context.set_keys_from_password(pwbox.value);
 
 savepwbutton.onclick=async ()=> context.export_keys_with_password(pwbox.value);

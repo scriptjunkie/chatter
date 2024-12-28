8ea7956ee12d0fe158457d58a6c002b12db225cc
scriptjunkie <scriptjunkie@scriptjunkie.us>
2024-12-28 14:24:51 -0500

    Add start chat button and merge JS loading options

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 import { context } from 'chattier';
 
-loadpwbutton.onclick=async ()=> context.set_keys_from_password(pwbox.value);
-
-savepwbutton.onclick=async ()=> context.export_keys_with_password(pwbox.value);
-
-console.log('starting');
-
 window.context = context;
+setupButtons();

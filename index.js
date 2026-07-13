6f51424e5c4b3939ba45bd20a275570e9422ca9d
scriptjunkie <scriptjunkie@scriptjunkie.us>
2026-07-12 21:58:51 -0400

    Save profile with password - warn if unsaved, add chat log saving

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import { context } from 'chattier';
+import { context, store_encrypted_object, load_encrypted_object } from 'chattier';
 
 window.context = context;
+window.store_encrypted_object = store_encrypted_object;
+window.load_encrypted_object = load_encrypted_object;
 setupButtons();
